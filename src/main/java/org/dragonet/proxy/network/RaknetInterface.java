b987b9393318e41bdfb6e7836a23a39869ec5e94
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-12-02 20:45:54 -0600

    Refactor RaknetInterface for better structure and readability

--- src/main/java/org/dragonet/proxy/network/RaknetInterface.java
+++ src/main/java/org/dragonet/proxy/network/RaknetInterface.java
@@ -1,112 +1,140 @@
+/*
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * You can view LICENCE file for details. 
+ *
+ * @author The Dragonet Team
+ */
 package org.dragonet.proxy.network;
 
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import lombok.Getter;
 import org.dragonet.net.packet.minecraft.BatchPacket;
 import org.dragonet.net.packet.minecraft.PEPacket;
 import org.dragonet.proxy.DragonProxy;
+import org.dragonet.proxy.configuration.Lang;
 import org.dragonet.proxy.utilities.Binary;
+import org.dragonet.proxy.utilities.Versioning;
+import org.dragonet.raknet.RakNet;
 import org.dragonet.raknet.protocol.EncapsulatedPacket;
 import org.dragonet.raknet.server.RakNetServer;
 import org.dragonet.raknet.server.ServerHandler;
 import org.dragonet.raknet.server.ServerInstance;
 
 public class RaknetInterface implements ServerInstance {
-   private final DragonProxy proxy;
-   private final SessionRegister sessions;
-   private final RakNetServer rakServer;
-   private final ServerHandler handler;
 
-   public RaknetInterface(DragonProxy proxy, String ip, int port) {
-      this.proxy = proxy;
-      this.rakServer = new RakNetServer(port, ip);
-      this.handler = new ServerHandler(this.rakServer, this);
-      this.sessions = this.proxy.getSessionRegister();
-   }
+    @Getter
+    private final DragonProxy proxy;
 
-   public void setBroadcastName(String serverName, int players, int maxPlayers) {
-      String name = "MCPE;";
-      name = name + serverName + ";";
-      name = name + "82;";
-      name = name + "0.15.10;";
-      name = name + players + ";" + maxPlayers;
-      if (this.handler != null) {
-         this.handler.sendOption("name", name);
-      }
+    private final SessionRegister sessions;
 
-   }
+    @Getter
+    private final RakNetServer rakServer;
 
-   public void onTick() {
-      while(this.handler.handlePacket()) {
-      }
+    @Getter
+    private final ServerHandler handler;
 
-   }
+    public RaknetInterface(DragonProxy proxy, String ip, int port) {
+        this.proxy = proxy;
+        rakServer = new RakNetServer(port, ip);
+        handler = new ServerHandler(rakServer, this);
+        sessions = this.proxy.getSessionRegister();
+    }
 
-   public void openSession(String identifier, String address, int port, long clientID) {
-      UpstreamSession session = new UpstreamSession(this.proxy, identifier, new InetSocketAddress(address, port));
-      this.sessions.newSession(session);
-   }
+    public void setBroadcastName(String serverName, int players, int maxPlayers) {
+        String name = "MCPE;";
+        name += serverName + ";";
+        name += Versioning.MINECRAFT_PE_PROTOCOL + ";";
+        name += Versioning.MINECRAFT_PE_VERSION + ";";
+        name += players + ";" + maxPlayers;
+        if (handler != null) {
+            handler.sendOption("name", name);
+        }
+    }
 
-   public void closeSession(String identifier, String reason) {
-      UpstreamSession session = this.sessions.getSession(identifier);
-      if (session != null) {
-         session.onDisconnect(this.proxy.getLang().get("message_client_disconnect"));
-      }
-   }
+    public void onTick() {
+        while (handler.handlePacket()) {
+        }
+    }
 
-   public void handleEncapsulated(String identifier, EncapsulatedPacket packet, int flags) {
-      UpstreamSession session = this.sessions.getSession(identifier);
-      if (session != null) {
-         packet.buffer = Arrays.copyOfRange(packet.buffer, 1, packet.buffer.length);
-         session.handlePacketBinary(packet);
-      }
-   }
+    @Override
+    public void openSession(String identifier, String address, int port, long clientID) {
+        UpstreamSession session = new UpstreamSession(proxy, identifier, new InetSocketAddress(address, port));
+        sessions.newSession(session);
+    }
 
-   public void handleRaw(String address, int port, byte[] payload) {
-   }
+    @Override
+    public void closeSession(String identifier, String reason) {
+        UpstreamSession session = sessions.getSession(identifier);
+        if (session == null) {
+            return;
+        }
+        session.onDisconnect(proxy.getLang().get(Lang.MESSAGE_CLIENT_DISCONNECT)); //It will handle rest of the things. 
+    }
 
-   public void notifyACK(String identifier, int identifierACK) {
-   }
+    @Override
+    public void handleEncapsulated(String identifier, EncapsulatedPacket packet, int flags) {
+        UpstreamSession session = sessions.getSession(identifier);
+        if (session == null) {
+            return;
+        }
+        packet.buffer = Arrays.copyOfRange(packet.buffer, 1, packet.buffer.length);
+        session.handlePacketBinary(packet);
+    }
 
-   public void handleOption(String option, String value) {
-   }
+    @Override
+    public void handleRaw(String address, int port, byte[] payload) {
+    }
 
-   public void shutdown() {
-      this.handler.shutdown();
-   }
+    @Override
+    public void notifyACK(String identifier, int identifierACK) {
+    }
 
-   public void disconnect(String identifier, String reason) {
-      this.handler.closeSession(identifier, reason);
-   }
+    @Override
+    public void handleOption(String option, String value) {
+    }
 
-   public void sendPacket(String identifier, PEPacket packet, boolean immediate) {
-      if (identifier != null && packet != null) {
-         boolean overridedImmediate = immediate || packet.isShouldSendImmidate();
-         packet.encode();
-         if (packet.getData().length > 512 && !BatchPacket.class.isAssignableFrom(packet.getClass())) {
+    public void shutdown() {
+        handler.shutdown();
+    }
+
+    public void disconnect(String identifier, String reason) {
+        handler.closeSession(identifier, reason);
+    }
+
+    public void sendPacket(String identifier, PEPacket packet, boolean immediate) {
+        if (identifier == null || packet == null) {
+            return;
+        }
+        
+        //Debug
+        /*
+        System.out.println("Sending [" + packet.getClass().getSimpleName() + "] after 2 seconds... ");
+        try{
+            Thread.sleep(2000L);
+        }catch(Exception e){}
+        */
+        
+        boolean overridedImmediate = immediate || packet.isShouldSendImmidate();
+        packet.encode();
+        if (packet.getData().length > 512 && !BatchPacket.class.isAssignableFrom(packet.getClass())) {
             BatchPacket pkBatch = new BatchPacket();
             pkBatch.packets.add(packet);
-            this.sendPacket(identifier, pkBatch, overridedImmediate);
-         } else {
-            EncapsulatedPacket encapsulated = new EncapsulatedPacket();
-            encapsulated.buffer = Binary.appendBytes((byte)-2, packet.getData());
-            encapsulated.needACK = true;
-            encapsulated.reliability = 2;
-            encapsulated.messageIndex = 0;
-            this.handler.sendEncapsulated(identifier, encapsulated, 8 | (overridedImmediate ? 1 : 0));
-         }
-      }
-   }
+            sendPacket(identifier, pkBatch, overridedImmediate);
+            return;
+        }
 
-   public DragonProxy getProxy() {
-      return this.proxy;
-   }
-
-   public RakNetServer getRakServer() {
-      return this.rakServer;
-   }
-
-   public ServerHandler getHandler() {
-      return this.handler;
-   }
+        EncapsulatedPacket encapsulated = new EncapsulatedPacket();
+        encapsulated.buffer = Binary.appendBytes((byte) 0xfe, packet.getData());
+        encapsulated.needACK = true;
+        encapsulated.reliability = (byte) 2;
+        encapsulated.messageIndex = 0;
+        handler.sendEncapsulated(identifier, encapsulated, RakNet.FLAG_NEED_ACK | (overridedImmediate ? RakNet.PRIORITY_IMMEDIATE : RakNet.PRIORITY_NORMAL));
+    }
 }
